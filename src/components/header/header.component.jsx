e70e3f927347e5b87bad29e25601a7adf3a00424
crazyoptimist <crazyoptimist@mail.com>
2021-04-16 17:35:10 -0500

    Add SIGNOUT link working, styles

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import './header.styles.scss';
-import { ReactComponent as Logo } from '../../assets/crown.svg';
 
-const Header = () => (
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+import { auth } from '../../firebase/firebase.utils';
+
+const Header = ({ currentUser }) => (
   <div className='header'>
     <Link to='/' className='logo-container'>
       <Logo className='logo' />
@@ -16,9 +18,12 @@ const Header = () => (
       <Link className='option' to='/contact'>
         CONTACT
       </Link>
-      <Link className='option' to='/signin'>
-        SIGN IN
-      </Link>
+      {
+        currentUser ? 
+          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+          :
+          <Link className='option' to='/signin'>SIGN IN</Link>
+      }
     </div>
   </div>
 );
