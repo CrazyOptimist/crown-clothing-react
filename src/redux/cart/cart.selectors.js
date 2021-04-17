d6382395da6cb88895fa5899139e8e43589ba6a1
crazyoptimist <crazyoptimist@mail.com>
2021-04-17 14:44:08 -0500

    Add checkout page skeleton, selector for cart total

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -5,7 +5,7 @@ const selectCart = state => state.cart;
 export const selectCartHidden = createSelector(
   [selectCart],
   (cart) => cart.hidden
-)
+);
 
 export const selectCartItems = createSelector(
   [selectCart],
@@ -15,4 +15,9 @@ export const selectCartItems = createSelector(
 export const selectCartItemsCount = createSelector(
   [selectCartItems],
   (cartItems) => cartItems.reduce(((acc, cartItem) => acc + cartItem.quantity), 0)
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  (cartItems) => cartItems.reduce(((acc, cartItem) => acc + cartItem.quantity * cartItem.price), 0)
 );
