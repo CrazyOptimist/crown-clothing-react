0704e8f461d64259b226f1894c115317c6c33357
crazyoptimist <crazyoptimist@mail.com>
2021-04-24 02:02:55 -0500

    Leverage dynamic nature of styled components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,7 +16,6 @@ import {
   HeaderContainer,
   LogoContainer,
   OptionsContainer,
-  OptionDiv,
   OptionLink
 } from './header.styles';
 
@@ -34,7 +33,7 @@ const Header = ({ currentUser, hidden }) => (
       </OptionLink>
       {
         currentUser ? 
-          <OptionDiv onClick={() => auth.signOut()}>SIGN OUT</OptionDiv>
+          <OptionLink as='div' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
           :
           <OptionLink to='/signin'>SIGN IN</OptionLink>
       }
