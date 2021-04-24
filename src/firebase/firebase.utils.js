2111984ef1a5244b7b7a647d1b34f43c6e0d98cd
crazyoptimist <crazyoptimist@mail.com>
2021-04-24 15:23:53 -0500

    Bring shop data into redux

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -42,4 +42,22 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 }
 
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  return transformedCollection.reduce((acc, collection) => {
+    acc[collection.title.toLowerCase()] = collection;
+    return acc;
+  }, {});
+}
+
 export default firebase;
