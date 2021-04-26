87d8360c48638795852ef7cb44520b434ddd22a3
crazyoptimist <crazyoptimist@mail.com>
2021-04-25 23:40:29 -0500

    :recycle:Refactor user sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,35 +1,43 @@
 import { takeLatest, put, all, call } from 'redux-saga/effects';
 
-import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+import {
+  auth,
+  googleProvider,
+  createUserProfileDocument
+} from '../../firebase/firebase.utils';
 
 import UserActionTypes from './user.types';
 
 import {
-  googleSignInSuccess,
-  googleSignInFailure,
-  emailSignInSuccess,
-  emailSignInFailure
+  signInSuccess,
+  signInFailure
 } from './user.actions';
+
+export function* getSnapshotFromUserAuth (userAuth) {
+  try {
+    const userRef = yield call(createUserProfileDocument, userAuth);
+    const userSnapshot = yield userRef.get();
+    yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+  } catch (error) {
+    yield put(signInFailure(error));
+  }
+}
 
 export function* signInWithGoogle() {
   try {
     const {user} = yield auth.signInWithPopup(googleProvider);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
 export function* signInWithEmail({payload: { email, password }}) {
   try {
     const { user } = yield auth.signInWithEmailAndPassword(email, password);
-    const userRef = yield call(createUserProfileDocument, user);
-    const userSnapshot = yield userRef.get();
-    yield put(emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
+    yield getSnapshotFromUserAuth(user);
   } catch (error) {
-    yield put(emailSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
