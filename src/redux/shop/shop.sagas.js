f35669490a1ad207141fe117e3b287a2d1efb490
crazyoptimist <crazyoptimist@mail.com>
2021-04-25 20:25:34 -0500

    Saga POC implemented

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,11 @@
+import { takeEvery } from 'redux-saga/effects';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionAsync() {
+  yield console.log("I'm being invoked!");
+}
+
+export function* fetchCollectionStart() {
+  yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionAsync)
+}
