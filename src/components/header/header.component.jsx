2ba4fa50cdf88a1e9f27ccfb67eca271ac05d75b
crazyoptimist <crazyoptimist@mail.com>
2021-04-27 00:26:36 -0500

    Add CurrentUser context and use Provider

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -6,38 +6,45 @@ import { createStructuredSelector } from 'reselect';
 import './header.styles.scss';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+
 import { auth } from '../../firebase/firebase.utils';
+
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
-const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link to='/' className='logo-container'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
+import CurrentUserContext from '../../contexts/current-user/current-user.context';
+
+const Header = ({ hidden }) => {
+  const currentUser = useContext(CurrentUserContext);
+
+  return (
+    <div className='header'>
+      <Link to='/' className='logo-container'>
+        <Logo className='logo' />
       </Link>
-      <Link className='option' to='/contact'>
-        CONTACT
-      </Link>
-      {
-        currentUser ? 
-          <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
-          :
-          <Link className='option' to='/signin'>SIGN IN</Link>
-      }
-      <CartIcon />
+      <div className='options'>
+        <Link className='option' to='/shop'>
+          SHOP
+        </Link>
+        <Link className='option' to='/contact'>
+          CONTACT
+        </Link>
+        {
+          currentUser ? 
+            <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div>
+            :
+            <Link className='option' to='/signin'>SIGN IN</Link>
+        }
+        <CartIcon />
+      </div>
+      {!hidden ? <CartDropdown /> : null}
     </div>
-    {!hidden ? <CartDropdown /> : null}
-  </div>
-);
+  );
+}
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
   hidden: selectCartHidden
 })
 
