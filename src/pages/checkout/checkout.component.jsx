5295a4d0ed0a1903d533f6d6ffcabe745f48036d
crazyoptimist <crazyoptimist@mail.com>
2021-04-27 03:34:44 -0500

    :recycle:Implement Provider pattern

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,51 +1,49 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from 'react';
 
-import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
 import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
+import { CartContext } from '../../providers/cart/cart.provider';
+
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
-  <div className='checkout-page'>
-    <div className='checkout-header'>
-      <div className='header-block'>
-        <span>Product</span>
-      </div>
-      <div className='header-block'>
-        <span>Description</span>
-      </div>
-      <div className='header-block'>
-        <span>Quantity</span>
-      </div>
-      <div className='header-block'>
-        <span>Price</span>
-      </div>
-      <div className='header-block'>
-        <span>Remove</span>
-      </div>
-    </div>
-    {
-      cartItems.map(cartItem => 
-        <CheckoutItem key={cartItem.id} cartItem={cartItem} />
-      )
-    }
-    <div className='total'>
-      <span>TOTAL: ${total}</span>
-    </div>
-    <div className='test-warning'>
-      *Please use a test credit card given here:<br />
-      https://stripe.com/docs/testing
-    </div>
-    <StripeCheckoutButton price={total} />
-  </div>
-);
+const CheckoutPage = () => {
+  const { cartItems, total } = useContext(CartContext);
 
-const mapStateToProps = createStructuredSelector({
-  cartItems: selectCartItems,
-  total: selectCartTotal
-});
+  return (
+    <div className='checkout-page'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
+      </div>
+      {
+        cartItems.map(cartItem => 
+          <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+        )
+      }
+      <div className='total'>
+        <span>TOTAL: ${total}</span>
+      </div>
+      <div className='test-warning'>
+        *Please use a test credit card given here:<br />
+        https://stripe.com/docs/testing
+      </div>
+      <StripeCheckoutButton price={total} />
+    </div>
+  );
+}
 
-export default connect(mapStateToProps)(CheckoutPage);
+export default CheckoutPage;
